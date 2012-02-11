4195bae7a4a57e1a76b9e60c555c2af995c1fd86
Xavier L <xavier.l@afrosoft.tk>
2012-02-11 17:20:54 -0500

    changed a few variables and updated readme file

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# No-IP uses emails as passwords, so make sure that you encode the @ as %40
 USERNAME=username
 PASSWORD=password
 HOST=hostsite
-DIR=$(pwd)
-CIPFILE=$DIR/current_ip.noip_updater
-USERAGENT="Simple bash noip updater/0.1 antoniocs@gmail.com"
+DIR=dir
+CIPFILE=$DIR/current_ip
+USERAGENT="Simple bash noip updater/0.2 antoniocs@gmail.com"
 
 if [ ! -e $CIPFILE ]
 then 
@@ -20,3 +21,4 @@ then
 	LOGFILE=$DIR/$(date +"%Y%m%d")_noip_updater.log		
 	wget -O "$LOGFILE" -q --user-agent="$USERAGENT" https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$IP	
 fi
+
