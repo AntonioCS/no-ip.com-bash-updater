a502520939cd8ef6ef2810887e9acd43058499d5
kousu <nick@kousu.ca>
2019-04-19 17:26:10 -0400

    Use no-ip's automatic IP detection instead of icanhazip.com.
    This removes the IP caching too, because it saves nothing:
    it avoided making a call to no-ip only by spending one to icanhazip.
    
    If the IP hasn't changed, no-ip returns "nochg $IP", and that'll get
    logged.

--- noipupdater.sh
+++ noipupdater.sh
@@ -7,40 +7,14 @@ HOST=hostsite
 LOGDIR=logdir             # best to use full absolute path (because of crontab integration)
 CONFIGDIR=configdir       # best to use full absolute path (because of crontab integration)
 LOGFILE=$LOGDIR/noip.log
-STOREDIPFILE=$CONFIGDIR/current_ip
 USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
 
 # create directories if not exist
 mkdir -p $LOGDIR
 mkdir -p $CONFIGDIR
 
-# create file to hold current ip
-if [ ! -e $STOREDIPFILE ]; then
-	touch $STOREDIPFILE
-fi
-
-# get current public ip from remote server
-NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
-
-# verify if dnsutils (dig command) is available
-if ! type "dig" > /dev/null; then
-  # no dig, using fallback on file storage
-  # on Debian/Ubuntu based systems, install package with: sudo apt install dnsutils
-  STOREDIP=$(cat $STOREDIPFILE)
-else
-  # use dig to get local dns record
-  STOREDIP=$(dig $HOST | grep $HOST | tail -n 1 | cut -f 5)
-fi
-
-# compare if ip has changed from the last time (avoiding unnecessary updates on no-ip)
-if [ "$NEWIP" != "$STOREDIP" ]; then
-	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
-
-	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
-	echo $NEWIP > $STOREDIPFILE
-else
-	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
-fi
+RESULT=$(wget -O "-" --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST")
+LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 
 echo $LOGLINE >> $LOGFILE
 
